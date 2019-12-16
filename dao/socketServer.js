2a231c7311d87561b5ccad62aaba9a145d036b08
Joseph Lee <development@jc-lab.net>
2019-12-16 20:48:22 +0900

    Add the ability to set key and cert directly without using a file in config

--- dao/socketServer.js
+++ dao/socketServer.js
@@ -269,7 +269,6 @@ function startServer() {
     );
 
     if(!this.udp4 && !this.udp6){
-        this.log('starting TLS server',this.config.tls);
         if(!this.config.tls){
             this.server=net.createServer(
                 serverCreated.bind(this)
@@ -346,29 +345,49 @@ function startServer() {
 }
 
 function startTLSServer(){
-    this.log('starting TLS server',this.config.tls);
-    if(this.config.tls.private){
-        this.config.tls.key=fs.readFileSync(this.config.tls.private);
-    }else{
-        this.config.tls.key=fs.readFileSync(`${__dirname}/../local-node-ipc-certs/private/server.key`);
+    const temp_tls = Object.assign({}, this.config.tls);
+    if(temp_tls.key) {
+        temp_tls.key = '#SECURE#';
     }
-    if(this.config.tls.public){
-        this.config.tls.cert=fs.readFileSync(this.config.tls.public);
-    }else{
-        this.config.tls.cert=fs.readFileSync(`${__dirname}/../local-node-ipc-certs/server.pub`);
+    if(temp_tls.cert) {
+        temp_tls.cert = '...';
     }
-    if(this.config.tls.dhparam){
-        this.config.tls.dhparam=fs.readFileSync(this.config.tls.dhparam);
+    if((typeof temp_tls.dhparam != 'string') || ((typeof temp_tls.dhparam === 'string') && !temp_tls.dhparam.startsWith("-----BEGIN DH PARAMETERS-----"))) {
+        temp_tls.dhparam = '#SECURE#';
+    }
+
+    this.log('starting TLS server',temp_tls);
+    if(!this.config.tls.key) {
+        if (this.config.tls.private) {
+            this.config.tls.key = fs.readFileSync(this.config.tls.private);
+        } else {
+            this.config.tls.key = fs.readFileSync(`${__dirname}/../local-node-ipc-certs/private/server.key`);
+        }
+    }
+    if(!this.config.tls.cert) {
+        if (this.config.tls.public) {
+            this.config.tls.cert = fs.readFileSync(this.config.tls.public);
+        } else {
+            this.config.tls.cert = fs.readFileSync(`${__dirname}/../local-node-ipc-certs/server.pub`);
+        }
+    }
+    if(this.config.tls.dhparam && (typeof this.config.tls.dhparam === 'string') && !this.config.tls.dhparam.startsWith("-----BEGIN DH PARAMETERS-----")) {
+        this.config.tls.dhparam = fs.readFileSync(this.config.tls.dhparam);
     }
     if(this.config.tls.trustedConnections){
         if(typeof this.config.tls.trustedConnections === 'string'){
             this.config.tls.trustedConnections=[this.config.tls.trustedConnections];
         }
         this.config.tls.ca=[];
-        for(let i=0; i<this.config.tls.trustedConnections.length; i++){
-            this.config.tls.ca.push(
-                fs.readFileSync(this.config.tls.trustedConnections[i])
-            );
+        for(let i=0; i<this.config.tls.trustedConnections.length; i++) {
+            const item = this.config.tls.trustedConnections[i];
+            if((typeof item === 'string') && !item.startsWith("-----BEGIN")) {
+                this.config.tls.ca.push(
+                    fs.readFileSync(this.config.tls.trustedConnections[i])
+                );
+            }else{
+                this.config.tls.ca.push(item);
+            }
         }
     }
     this.server=tls.createServer(
